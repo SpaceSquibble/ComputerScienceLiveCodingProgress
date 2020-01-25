93a233f85d724b66be3a3e08520f619429258507
SpaceSquibble <54996465+SpaceSquibble@users.noreply.github.com>
2020-01-24 20:44:05 -0500

    kinda giving up rn tbh (•__•)
    all I did was change the radius and add another heart and change the speed.
    
    I tried adding more parts to the object. start positions, and another var to track the speed
    
    WHY ISNT THIS WORKING!!>?

--- sketch.js
+++ sketch.js
@@ -1,79 +1,67 @@
-//version 3 of the code
+//version 4 of the code
 
 //defining our variables
-
 let r; //radius
 let factor = 0; //times table ex2 times table makes the carioid shape 3 times table makes hetroid or something
 let total;
-//let startx;
-//let starty;
 
-function Heart(startx, starty)
-{
-  this.startx= startx;
-  this.starty= starty;
+function Heart(startx, starty, position) {
+  this.startx = startx;
+  this.starty = starty;
+  this.position = position;
 }
 
-/*
-//get vector function used to make the line in the draw function
-function getVector(index, total) {
-  const angle = map(index % total, 0, total, 0, TWO_PI);
-  const v = p5.Vector.fromAngle(angle + PI);
-  v.mult(r);
-  return v;
-}
-*/
-
-Heart.prototype.output = function()
-{
+Heart.prototype.output = function() {
   translate(width / 2, height / 2); //translating our shape
-  stroke(mouseX,mouseY, 30); //making the colour change depending on where the mouse is
-  strokeWeight(2); //making the stroke weight 2
+  stroke(mouseX, mouseY, 30); //making the colour change depending on where the mouse is
+  strokeWeight(1); //making the stroke weight 2
   noFill(); // we wanna see the lines so no colour in the ellipse
   ellipse(startx, starty, r * 2); //outputting a circle
 }
 
-Heart.prototype.lines = function()
-{
+Heart.prototype.lines = function() {
   const total = 50; //int(map(mouseX, 0, width, 0, 200));
-  factor += 0.015;
+  factor += 0.004; //controls the speed (multiply by 2, then 2.004, then 2.008 etc)
   
-  strokeWeight(1);
+  strokeWeight(2);
   for (let i = 0; i < total; i++) {
     const a = getVector(i, total); // calculate first point (fixed positions)
     const b = getVector(i * factor, total); //calculate second point (moving positions due to the *factor)
     //line(a.x, a.y , b.x , b.y );
     line(a.x + startx, a.y + starty, b.x + startx, b.y + starty); //draws the line
+
   }
 }
-  
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  r = height / 2 - 16;
-  startx = 0;
-  starty = 0;
+  r = height / 4; //radius of the circle
+  startx = 0; //start x position
+  starty = 0; //start y position
 }
-  
+
+//get vector function used to make the line in the draw function
 function getVector(index, total) {
-  const angle = map(index % total, 0, total, 0, TWO_PI);
+  const angle = map(index % total, 0, total, 0, TWO_PI); //calculated the angle that the line will be at
   const v = p5.Vector.fromAngle(angle + PI);
-  v.mult(r);
+  v.mult(r); //holds that angle after multiplying it by the radius
   return v;
 }
 
-var heart1 = new Heart(10, 10);
-var heart2 = new Heart(10, 10);
-var heart3 = new Heart(10, 10);
+
+var heart1 = new Heart(10, 10, 1);
+var heart2 = new Heart(10, 10, 3);
+var heart3 = new Heart(10, 10, 2);
 
 function draw() {
   background(0);
 
   heart1.output();
   heart1.lines();
-  
+
   //heart2.output();
   //heart2.lines();
-  
+
   heart3.output();
   heart3.lines();
 }
