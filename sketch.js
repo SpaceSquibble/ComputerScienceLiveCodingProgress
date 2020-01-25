35f808972c78734cfd2d1b8dd65f276f9511a0d8
SpaceSquibble <54996465+SpaceSquibble@users.noreply.github.com>
2020-01-24 20:31:30 -0500

    object oriented!
    I Made it object oriented!!!
    
    - 2 shapes are outputted, 
    - they don't move yet 
    - their positions are kinda off. If I start both at (10,10), thewy appear in different positions which shouldn't happen ;/

--- sketch.js
+++ sketch.js
@@ -1,20 +1,14 @@
-//version 2 of the code
+//version 3 of the code
 
 //defining our variables
-let r;
-let factor = 0;
-let startx;
-let starty;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight); //making a new window
-  r = height / 2; //settoing the radius
-  startx = 0; //starting x position
-  starty = 0; //starting y position
-}
+let r; //radius
+let factor = 0; //times table ex2 times table makes the carioid shape 3 times table makes hetroid or something
+let total;
+//let startx;
+//let starty;
 
-//object for heart
-function heart (startx, starty)
+function Heart(startx, starty)
 {
   this.startx= startx;
   this.starty= starty;
@@ -30,28 +24,56 @@ function getVector(index, total) {
 }
 */
 
-//drawing the circle and lines
-heart.prototype.display()
+Heart.prototype.output = function()
 {
-   translate(width / 2, height / 2); //translating our shape
+  translate(width / 2, height / 2); //translating our shape
   stroke(mouseX,mouseY, 30); //making the colour change depending on where the mouse is
   strokeWeight(2); //making the stroke weight 2
   noFill(); // we wanna see the lines so no colour in the ellipse
   ellipse(startx, starty, r * 2); //outputting a circle
+}
 
-  strokeWeight(2);
+Heart.prototype.lines = function()
+{
+  const total = 50; //int(map(mouseX, 0, width, 0, 200));
+  factor += 0.015;
+  
+  strokeWeight(1);
   for (let i = 0; i < total; i++) {
     const a = getVector(i, total); // calculate first point (fixed positions)
     const b = getVector(i * factor, total); //calculate second point (moving positions due to the *factor)
+    //line(a.x, a.y , b.x , b.y );
     line(a.x + startx, a.y + starty, b.x + startx, b.y + starty); //draws the line
   }
 }
-
-var heart1= new heart(height / 2 - 16);
-
-draw = function()
-{
-  background(0);
   
-  heart1.display();
-};
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  r = height / 2 - 16;
+  startx = 0;
+  starty = 0;
+}
+  
+function getVector(index, total) {
+  const angle = map(index % total, 0, total, 0, TWO_PI);
+  const v = p5.Vector.fromAngle(angle + PI);
+  v.mult(r);
+  return v;
+}
+
+var heart1 = new Heart(10, 10);
+var heart2 = new Heart(10, 10);
+var heart3 = new Heart(10, 10);
+
+function draw() {
+  background(0);
+
+  heart1.output();
+  heart1.lines();
+  
+  //heart2.output();
+  //heart2.lines();
+  
+  heart3.output();
+  heart3.lines();
+}
