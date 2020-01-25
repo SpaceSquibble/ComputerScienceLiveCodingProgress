37eb70ddf6b4caa12839d223f3536285cd2656da
SpaceSquibble <54996465+SpaceSquibble@users.noreply.github.com>
2020-01-24 20:50:24 -0500

    ITS WORKING HECK YEAH
    THEYRE MOVING YES!!!!!
    
    it works!!!!!!!!! they both move in the same direction tho. I might be able to fix that by using acceleration, velocity etc  like we did in khanacademy.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,13 @@
-//version 4 of the code
+//version 5 of the code
 
 //defining our variables
 let r; //radius
 let factor = 0; //times table ex2 times table makes the carioid shape 3 times table makes hetroid or something
 let total;
 
-function Heart(startx, starty, position) {
+function Heart(startx, starty) {
   this.startx = startx;
   this.starty = starty;
-  this.position = position;
 }
 
 Heart.prototype.output = function() {
@@ -18,24 +17,35 @@ Heart.prototype.output = function() {
   noFill(); // we wanna see the lines so no colour in the ellipse
   ellipse(startx, starty, r * 2); //outputting a circle
 }
-
-Heart.prototype.lines = function() {
-  const total = 50; //int(map(mouseX, 0, width, 0, 200));
-  factor += 0.004; //controls the speed (multiply by 2, then 2.004, then 2.008 etc)
   
-  strokeWeight(2);
+Heart.prototype.lines = function() {
+  const total = 30; //int(map(mouseX, 0, width, 0, 200));
+  factor += 0.004; //controls the speed (multiply by 2, then 2.004, then 2.008 etc)
+
+  strokeWeight(1);
   for (let i = 0; i < total; i++) {
     const a = getVector(i, total); // calculate first point (fixed positions)
     const b = getVector(i * factor, total); //calculate second point (moving positions due to the *factor)
     //line(a.x, a.y , b.x , b.y );
     line(a.x + startx, a.y + starty, b.x + startx, b.y + starty); //draws the line
+    
+    //ellipse(this.startx+r,this.starty,5);
 
+    if(startx+r <width)
+    {
+      ellipse(startx+r,starty,5);
+      startx = startx +0.02;
+    }
+    else
+    {
+      startx= -startx;
+    }
   }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  r = height / 4; //radius of the circle
+  r = height / 4 ; //radius of the circle
   startx = 0; //start x position
   starty = 0; //start y position
 }
@@ -48,10 +58,10 @@ function getVector(index, total) {
   return v;
 }
 
-
-var heart1 = new Heart(10, 10, 1);
-var heart2 = new Heart(10, 10, 3);
-var heart3 = new Heart(10, 10, 2);
+var heart1 = new Heart(10, 10);
+var heart2 = new Heart(30, 7);
+var heart3 = new Heart(20, 20);
+var heart4 = new Heart(-10, -10);
 
 function draw() {
   background(0);
@@ -59,9 +69,12 @@ function draw() {
   heart1.output();
   heart1.lines();
 
-  //heart2.output();
-  //heart2.lines();
+  heart2.output();
+  heart2.lines();
 
   heart3.output();
   heart3.lines();
+  
+  heart4.output();
+  heart4.lines();
 }
